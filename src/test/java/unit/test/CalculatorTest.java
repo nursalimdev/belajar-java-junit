620e55b4978b02eecd56013978a47aee2885b975
nursalimdev <nursalimdev123@gmail.com>
2023-02-01 09:08:41 +0700

    commit

--- src/test/java/unit/test/CalculatorTest.java
+++ src/test/java/unit/test/CalculatorTest.java
@@ -1,11 +1,37 @@
 package unit.test;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import unit.test.generator.SimpleDisplayNameGenerator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+//@DisplayName("Test untuk Calculator")
+@DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("beforeAll");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("afterAll");
+    }
+
+    @BeforeEach
+    public void setUp(){
+        System.out.println("Sebelum unit test dijalankan");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        System.out.println("Setelah unit test dijalankan");
+    }
+
     @Test
+//    @DisplayName("Test skenario sukses untuk method add(integer, integer)")
     public void testAddSuccess(){
         var result = calculator.add(10, 10);
 
@@ -26,7 +52,13 @@ public class CalculatorTest {
     @Test
     public void testDivideFailed(){
         assertThrows(IllegalArgumentException.class, () -> {
-           calculator.divide(10, 10);
+           calculator.divide(10, 0);
         });
+    }
+
+    @Test
+    @Disabled
+    public void testComingSoon(){
+        //TODO
     }
 }
