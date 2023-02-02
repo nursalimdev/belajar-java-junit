c3c139d20973f98dec132664e76e26b17a24cf6d
nursalimdev <nursalimdev123@gmail.com>
2023-02-02 13:57:46 +0700

    commit

---
+++ src/test/java/unit/test/QueueTest.java
@@ -0,0 +1,36 @@
+package unit.test;
+
+import org.junit.jupiter.api.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@DisplayName("A Queue")
+public class QueueTest {
+    private Queue<String> queue;
+
+    @Nested
+    @DisplayName("When New")
+    public class WhenNew{
+
+        @BeforeEach
+        void setUp(){
+            queue = new LinkedList<>();
+        }
+
+        @Test
+        @DisplayName("when offer, size must be 1")
+        void offerNewData(){
+            queue.offer("Nursalim");
+            Assertions.assertEquals(1, queue.size());
+        }
+
+        @Test
+        @DisplayName("when offer 2 data, size must be 2")
+        void offerMoreData(){
+            queue.offer("Nursalim");
+            queue.offer("Al Farizi");
+            Assertions.assertEquals(2, queue.size());
+        }
+    }
+}
